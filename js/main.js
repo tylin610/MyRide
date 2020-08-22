631c1d4c5decef3887c8b50fe1eef1689a18549a
licht <neon.1598@mail.ru>
2020-08-22 18:12:51 +0600

    slides title

--- js/main.js
+++ js/main.js
@@ -209,32 +209,57 @@ $(function() {
         }
     });
 
+
     new fullpage('#fullpage', {
         anchors: ['page1', 'page2', 'page3', 'page4'],
-        afterLoad: function(origin){
+        afterLoad: function(origin, destination, direction){
             var loadedSection = this;
+            var indexOfSection = destination.index + 1;
+            $('.section').removeClass('effect');
+            $('.section').removeClass('after-effect');
+            $('.section:nth-child(' + indexOfSection + ')').addClass('effect');
+
+            if(indexOfSection > 2 && indexOfSection < 10 ) {
+                $('.sections-title1').fadeIn(500);
+                setTimeout(function() {
+                    $('.section4__inner1').fadeIn();
+                }, 3000);
+            } else {
+                $('.sections-title1').fadeOut();
+            }
+
             if($('.section1').hasClass('active')){
                 $('.header').slideUp();
             } else {
                 $('.header').slideDown();
             }
         },
-        // navigation: true,
-        // navigationPosition: 'right',
-        // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+        onLeave: function(origin, destination, direction){
+            var leavingSection = origin.index + 1;
+            $('.section:nth-child(' + leavingSection + ')').removeClass('effect');
+            $('.section:nth-child(' + leavingSection + ')').addClass('after-effect');
+            if(leavingSection >= 2 && leavingSection <= 9 ) {
+                $('.sections-title1').fadeIn(500);
+            }
+            if(leavingSection == 9 && direction == 'down') {
+                $('.sections-title1').fadeOut(500);
+            }
+            console.log(leavingSection);
+        },
         css3: true,
         scrollingSpeed: 1000,
         responsiveHeight: 330,
         dragAndMove: true,
         dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
         controlArrows: false,
-        parallax: true,
-        parallaxKey: 'YWx2YXJvdHJpZ28uY29tXzlNZGNHRnlZV3hzWVhnPTFyRQ==',
-        parallaxOptions: {
-            type: 'reveal',
-            percentage: 62,
-            property: 'translate'
-        },
+        // navigation: true,
+        // navigationPosition: 'right',
+        // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
     });
+
+    // $('.sections-title1').css({
+    //     '' : '',
+    // });
+
 
 });
