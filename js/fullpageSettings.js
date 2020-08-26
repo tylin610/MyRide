b8c05029bb1e86b57548114c0ac5c7de6f90a9bf
licht <neon.1598@mail.ru>
2020-08-26 14:58:02 +0600

    all titles + few section

--- js/fullpageSettings.js
+++ js/fullpageSettings.js
@@ -17,9 +17,28 @@ $(function () {
                 $('.sections-title1').addClass('animated');
             }
 
+            if (indexOfSection > 9 && indexOfSection < 11) {
+                $('.sections-title2').fadeIn();
+                $('.sections-title2').addClass('animated');
+            }
+
+            if (indexOfSection == 13) {
+                $('.sections-title3').fadeIn();
+                $('.sections-title3').addClass('animated');
+            }
+
             if (indexOfSection >= 3 && indexOfSection <= 9) {
                 $('.sections-title1').addClass('transp');
                 $('.sections-subtitle1').fadeIn();
+            }
+            if (indexOfSection >= 11 && indexOfSection <= 12) {
+                $('.sections-title2').fadeIn();
+                $('.sections-title2').addClass('transp');
+            }
+
+            if (indexOfSection >= 14 && indexOfSection <= 15) {
+                $('.sections-title3').fadeIn();
+                $('.sections-title3').addClass('transp');
             }
 
             if ($('.section1').hasClass('active')) {
@@ -52,12 +71,33 @@ $(function () {
 
             if (leavingSection == 6 && direction == 'up') {
                 $('.sections-subtitle1').fadeIn();
+                $('.sections-subtitle1').addClass('animated');
+                $('.sections-subtitle1').removeClass('after-animated');
                 $('.sections-subtitle2').fadeOut();
             }
 
             if (leavingSection == 8 && direction == 'up') {
                 $('.sections-subtitle2').fadeIn();
+                $('.sections-subtitle2').addClass('animated');
+                $('.sections-subtitle2').removeClass('after-animated');
                 $('.sections-subtitle3').fadeOut();
+            }
+
+            if (leavingSection == 10 && direction == 'up') {
+                $('.sections-title2').fadeOut();
+            }
+
+            if (leavingSection == 11 && direction == 'up') {
+                $('.sections-title2').removeClass('transp');
+            }
+
+            if (leavingSection == 13 && direction == 'up') {
+                $('.sections-title3').fadeOut();
+                $('.sections-subtitle1').removeClass('animated');
+            }
+
+            if (leavingSection == 14 && direction == 'up') {
+                $('.sections-title3').removeClass('transp');
             }
 
             if (leavingSection == 2 && direction == 'down') {
@@ -66,6 +106,7 @@ $(function () {
 
             if (leavingSection == 5 && direction == 'down') {
                 $('.sections-subtitle1').removeClass('animated');
+                $('.sections-subtitle1').addClass('after-animated');
                 $('.sections-subtitle1').fadeOut();
                 $('.sections-subtitle2').fadeIn();
                 $('.sections-subtitle2').addClass('animated');
@@ -73,6 +114,7 @@ $(function () {
 
             if (leavingSection == 7 && direction == 'down') {
                 $('.sections-subtitle2').removeClass('animated');
+                $('.sections-subtitle2').addClass('after-animated');
                 $('.sections-subtitle2').fadeOut();
                 $('.sections-subtitle3').fadeIn();
                 $('.sections-subtitle3').addClass('animated');
@@ -82,6 +124,19 @@ $(function () {
                 $('.sections-title1').fadeOut();
                 $('.sections-subtitle3').fadeOut();
             }
+
+            if (leavingSection == 10 && direction == 'down') {
+                $('.sections-title2').addClass('transp');
+            }
+            if (leavingSection == 12 && direction == 'down') {
+                $('.sections-title2').fadeOut();
+            }
+            if (leavingSection == 13 && direction == 'down') {
+                $('.sections-title3').addClass('transp');
+            }
+            if (leavingSection == 15 && direction == 'down') {
+                $('.sections-title3').fadeOut();
+            }
         },
         css3: true,
         scrollingSpeed: 1000,
@@ -89,8 +144,8 @@ $(function () {
         dragAndMove: true,
         dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
         controlArrows: false,
-        // navigation: true,
-        // navigationPosition: 'right',
+        navigation: true,
+        navigationPosition: 'right',
         // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
     });
 
