0d7dd6b3023f4201fd3816eb632eb132711b9946
licht <imatilyn.888@mail.ru>
2020-09-10 01:02:14 +0600

    feat: added points to main page

--- js/fullpageSettings.js
+++ js/fullpageSettings.js
@@ -9,7 +9,6 @@ $(function () {
             $('.section:nth-child(' + indexOfSection + ')').addClass('effect');
             
             if (indexOfSection == 12) {
-                console.log(indexOfSection);
                 var slideIndex = 1;
 
                 function plusSlide() {
@@ -108,12 +107,49 @@ $(function () {
 
             if (indexOfSection > 2 && indexOfSection < 6) {
                 $('.sections-subtitle1').addClass('animated');
+            } else {
+                $('.sections-subtitle1').fadeOut();
+                $('.sections-subtitle1').removeClass('animated');
             }
+            if (indexOfSection > 5 && indexOfSection < 8) {
+                $('.sections-subtitle2').addClass('animated');
+            } else {
+                $('.sections-subtitle2').fadeOut();
+                $('.sections-subtitle2').removeClass('animated');
+            }
+            if (indexOfSection > 7 && indexOfSection < 10) {
+                $('.sections-subtitle3').addClass('animated');
+            } else {
+                $('.sections-subtitle3').fadeOut();
+                $('.sections-subtitle3').removeClass('animated');
+            }
+
+            if (indexOfSection > 9 || indexOfSection < 2) {
+                $('.sections-title1').fadeOut();
+            } 
+            if (indexOfSection > 12 || indexOfSection < 9) {
+                $('.sections-title2').fadeOut();
+            } 
+            if (indexOfSection > 15 || indexOfSection < 12) {
+                $('.sections-title3').fadeOut();
+            } 
+            if (indexOfSection > 17 || indexOfSection < 15) {
+                $('.sections-title4').fadeOut();
+            } 
+            if (indexOfSection > 19 || indexOfSection < 17) {
+                $('.sections-title5').fadeOut();
+            } 
+            if (indexOfSection > 21 || indexOfSection < 19) {
+                $('.sections-title6').fadeOut();
+            } 
+
             if (indexOfSection > 1 && indexOfSection < 10) {
                 $('.sections-title1').fadeIn();
                 $('.sections-title1').addClass('animated');
                 fullpage_api.setScrollingSpeed(500);
             }
+
+            console.log(indexOfSection);
 
             if (indexOfSection > 9 && indexOfSection < 11) {
                 $('.sections-title2').fadeIn();
@@ -125,6 +161,7 @@ $(function () {
 
             if (indexOfSection >= 3 && indexOfSection <= 9) {
                 $('.sections-title1').addClass('transp');
+                $('.sections-title1').addClass('animated');
                 $('.sections-subtitle1').fadeIn();
             }
 
@@ -135,12 +172,32 @@ $(function () {
 
             if (indexOfSection >= 11 && indexOfSection <= 12) {
                 $('.sections-title2').fadeIn();
+                $('.sections-title2').addClass('animated');
                 $('.sections-title2').addClass('transp');
             }
 
             if (indexOfSection >= 14 && indexOfSection <= 15) {
                 $('.sections-title3').fadeIn();
+                $('.sections-title3').addClass('animated');
                 $('.sections-title3').addClass('transp');
+            }
+
+            if (indexOfSection >= 17 && indexOfSection <= 18) {
+                $('.sections-title4').fadeIn();
+                $('.sections-title4').addClass('animated');
+                $('.sections-title4').addClass('transp');
+            }
+
+            if (indexOfSection >= 20 && indexOfSection <= 20) {
+                $('.sections-title5').fadeIn();
+                $('.sections-title5').addClass('animated');
+                $('.sections-title5').addClass('transp');
+            }
+
+            if (indexOfSection >= 22 && indexOfSection <= 22) {
+                $('.sections-title6').fadeIn();
+                $('.sections-title6').addClass('animated');
+                $('.sections-title6').addClass('transp');
             }
 
             stoppedSection(13, '.sections-title3', 'fadeIn');
@@ -162,6 +219,53 @@ $(function () {
             stoppedSection(19, '.sections-title5', 'addClass', 'transp');
 
             stoppedSection(21, '.sections-title6', 'fadeIn');
+            stoppedSection(21, '.sections-title6', 'addClass', 'transp');
+
+            stoppedSection(1, '.sections-title1', 'fadeOut');
+            stoppedSection(2, '.sections-title1', 'removeClass', 'transp');
+            stoppedSection(2, '.sections-subtitle1', 'removeClass', 'animated');
+            stoppedSection(5, '.sections-subtitle1', 'fadeIn');
+            stoppedSection(5, '.sections-subtitle1', 'addClass', 'animated');
+            stoppedSection(5, '.sections-subtitle1', 'removeClass', 'after-animated');
+            stoppedSection(5, '.sections-subtitle2', 'fadeOut');
+            stoppedSection(7, '.sections-subtitle2', 'fadeIn');
+            stoppedSection(7, '.sections-subtitle2', 'addClass', 'animated');
+            stoppedSection(7, '.sections-subtitle2', 'removeClass', 'after-animated');
+            stoppedSection(7, '.sections-subtitle3', 'fadeOut');
+            stoppedSection(9, '.sections-title2', 'fadeOut');
+            stoppedSection(10, '.sections-title2', 'removeClass', 'transp');
+            stoppedSection(12, '.sections-title3', 'fadeOut');
+            stoppedSection(12, '.sections-subtitle1', 'removeClass', 'animated');
+            stoppedSection(13, '.sections-title3', 'removeClass', 'transp');
+            stoppedSection(15, '.sections-title4', 'fadeOut');
+            stoppedSection(17, '.sections-title5', 'fadeOut');
+            stoppedSection(19, '.sections-title6', 'fadeOut');
+            stoppedSection(16, '.sections-title4', 'removeClass', 'transp');
+            stoppedSection(18, '.sections-title5', 'removeClass', 'transp');
+            stoppedSection(20, '.sections-title6', 'removeClass', 'transp');
+
+            stoppedSection(3, '.sections-title1', 'addClass', 'transp');
+            stoppedSection(6, '.sections-subtitle1', 'removeClass', 'animated');
+            stoppedSection(8, '.sections-subtitle2', 'removeClass', 'animated');
+            stoppedSection(6, '.sections-subtitle2', 'addClass', 'animated');
+            stoppedSection(8, '.sections-subtitle3', 'addClass', 'animated');
+            stoppedSection(6, '.sections-subtitle1', 'addClass', 'after-animated');
+            stoppedSection(8, '.sections-subtitle2', 'addClass', 'after-animated');
+            stoppedSection(6, '.sections-subtitle1', 'fadeOut');
+            stoppedSection(8, '.sections-subtitle2', 'fadeOut');
+            stoppedSection(10, '.sections-subtitle3', 'fadeOut');
+            stoppedSection(6, '.sections-subtitle2', 'fadeIn');
+            stoppedSection(8, '.sections-subtitle3', 'fadeIn');
+            stoppedSection(10, '.sections-title1', 'fadeOut');
+            stoppedSection(13, '.sections-title2', 'fadeOut');
+            stoppedSection(16, '.sections-title3', 'fadeOut');
+            stoppedSection(18, '.sections-title4', 'fadeOut');
+            stoppedSection(20, '.sections-title5', 'fadeOut');
+            stoppedSection(22, '.sections-title6', 'fadeOut');
+            stoppedSection(11, '.sections-title2', 'addClass', 'transp');
+            stoppedSection(14, '.sections-title3', 'addClass', 'transp');
+            stoppedSection(17, '.sections-title4', 'addClass', 'transp');
+            stoppedSection(19, '.sections-title5', 'addClass', 'transp');
             stoppedSection(21, '.sections-title6', 'addClass', 'transp');
 
             if ($('.section1').hasClass('active')) {
@@ -283,9 +387,15 @@ $(function () {
         dragAndMove: true,
         dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
         controlArrows: false,
-        // navigation: true,
-        // navigationPosition: 'right',
-        // navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: [
+            'MYRIDE', 'MYRIDE - ЭТО ШОУ', 'Потрясающие ощущения', 'Потрясающие ощущения','Потрясающие ощущения',
+            '100% драйв ночного клуба', '100% драйв ночного клуба', 'Атмосфера вдохновения', 'Атмосфера вдохновения',
+            'MYRIDE - ЭТО ЭФФЕКТИВНО', 'ЗАНЯТИЯ', 'ПОЛЬЗА', 'MYRIDE - ЭТО ИНДИВИДУАЛЬНО',
+            'БАЙК', 'ПОКАЗАТЕЛИ', 'КАЖДОМУ ГОСТЮ БЕСПЛАТНО ВЫДАЮТСЯ', 'КАЖДОМУ ГОСТЮ БЕСПЛАТНО ВЫДАЮТСЯ',
+            'НАШИ ТРЕНЕРЫ', 'ТРЕНЕРЫ', 'ЭКСКЛЮЗИВНАЯ ЗОНА SPA', 'SPA-КОМПЛЕКС', 'КОНТАКТЫ'
+        ],
     });
 
     // $('.sections-title1').css({
